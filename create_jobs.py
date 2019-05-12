be2bb35f75e6870573c3b45efd5b6366787fe3f2
Grigory Chirkov <gchirkov@princeton.edu>
2019-05-12 12:57:43 -0400

    small fix in create jobs scrips

--- create_jobs.py
+++ create_jobs.py
@@ -10,11 +10,7 @@ for n in [1,2,4,8,16,32,64]:
             for g in [0,1,2,3,4]:
                 with open("jobs/%s_g%d_n%d_d%d.slurm" % (t, g, n, d), "w+") as f:
                     f.write("#!/bin/bash\n")
-                    f.write("#SBATCH --nodes=1\n")
-                    if n <= 16:
-                        f.write("#SBATCH -B 1:16\n")
-                    else:
-                        f.write("#SBATCH -B 2:16\n")
+                    f.write("#SBATCH -B 1:4\n")
                     f.write("#SBATCH --time=20:00:00\n")
                     f.write("#SBATCH --mail-type=begin\n")
                     f.write("#SBATCH --mail-type=end\n")
