64d9ee9fbbbcd4d4c6a7b0cd0a4271f9bfe19d26
Grigory Chirkov <gchirkov@princeton.edu>
2019-05-12 17:18:55 -0400

    fix bug with multiple writes to same files

--- create_jobs.py
+++ create_jobs.py
@@ -28,8 +28,22 @@ for n in [1,2,4,8,16,32,64]:
                 f.write("source env.sh\n")
                 f.write("\n")
 
+                f.write("if [ ! -d \"%s/../parsec_data\"  ]; then\n" % curdir)
+                f.write("\tmkdir %s/../parsec_data\n" % curdir)
+                f.write("fi\n")
+                f.write("\n")
+
+                f.write("if [ ! -d \"%s/../parsec_data/%s_n%d_d%d\"  ]; then\n" % (curdir, t,n,d))
+                f.write("\tcp -r %s/../parsec_data_template  %s/../parsec_data/%s_n%d_d%d\n" % (curdir, curdir, t,n,d))
+                f.write("fi\n")
+
+
                 for b in b_grouped[g]:
-                    f.write("run_prime -c %s/xml/%s_d%d.xml " % (curdir, t,d))
-                    f.write("-o %s/output/%s_%s_n%d_d%d.out " % (curdir, b,t,n,d))
-                    f.write("%s,%d,simsmall " % (b,n))
-                    f.write("> %s/logs/%s_%s_n%d_d%d.log \n" % (curdir,b,t,n,d))
+                    f.write("run_prime -c %s/xml/%s_d%d.xml "  % (curdir, t,d))
+                    f.write("-o %s/output/%s_%s_n%d_d%d.out "  % (curdir, b,t,n,d))
+                    f.write("-d %s/../parsec_data/%s_n%d_d%d " % (curdir, t,n,d))
+                    if n == 1:
+                        f.write("%s,%d,simsmall " % (b,0))
+                    else:
+                        f.write("%s,%d,simsmall " % (b,n))
+                    f.write("> %s/logs/%s_%s_n%d_d%d.log \n"   % (curdir,b,t,n,d))
